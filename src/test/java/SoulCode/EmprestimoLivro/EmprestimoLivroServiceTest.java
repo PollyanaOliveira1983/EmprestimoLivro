f2682e5814e06dd393c83701d5b3f9ba3c79e39a
Pollyana Oliveira <90224120+PollyanaOliveira1983@users.noreply.github.com>
2022-05-06 14:07:40 -0300

    Projeto finalizado

--- src/test/java/SoulCode/EmprestimoLivro/EmprestimoLivroServiceTest.java
+++ src/test/java/SoulCode/EmprestimoLivro/EmprestimoLivroServiceTest.java
@@ -3,8 +3,11 @@ package SoulCode.EmprestimoLivro;
 import SoulCode.EmprestimoLivro.Model.EmprestimoLivro;
 import SoulCode.EmprestimoLivro.Repository.EmprestimoLivroRepository;
 import SoulCode.EmprestimoLivro.Service.EmprestimoLivroService;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 //faz a ponte entre o spring boot e JUnit
 @RunWith(SpringRunner.class)
@@ -38,13 +42,20 @@ public class EmprestimoLivroServiceTest {
     @Test
     public void emprestimoLivroServiceCalculoDeDias(){
 
-    }
+        String nomeReserva = "Thyago";
+        int dias = emprestimoLivroService.calculoDeDias(nomeReserva);
 
+        Assertions.assertEquals(dias,10);
+    }
+    @Before
     public void setup(){
         LocalDate dataSaida = LocalDate.parse("2022-05-10");
         LocalDate dataDevolucao = LocalDate.parse("2022-05-20");
 
         EmprestimoLivro emprestimoLivro = new EmprestimoLivro("1","Thyago", dataSaida,dataDevolucao, "Kafta");
-    }
 
+        //quando for preciso um registro(objeto de dados) para simular o teste
+        Mockito.when(emprestimoLivroRepository.findByNomeDaReserva(emprestimoLivro.getNomeDaReserva()))
+                .thenReturn(Optional.of(emprestimoLivro));
+    }
 }
